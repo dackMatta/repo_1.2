d604502e4551dd39c4f020335217be7c44f4737c
Entrix Makambi <160459342+dackMatta@users.noreply.github.com>
2024-02-28 12:28:31 +0300

    Add files via upload
    week two python assignment

---
+++ my_list.py
@@ -0,0 +1,15 @@
+my_list=[]
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(2,15)
+myb_list=[50,60,70]
+my_list.extend(myb_list)
+my_list.remove(70)
+my_list.sort()
+my_list.index(30)
+print(my_list)
+
+
+
